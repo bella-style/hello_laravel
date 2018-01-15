bdbd6e1c3563ef8cc8f32f61c12fe64ec8b47dae
Bella <1162509783@qq.com>
2018-01-15 23:36:46 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+              margin: 0;
+              padding: 0;
+              width: 100%;
+              display: table;
+              font-weight: 100;
+              font-family: 'Lato';
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+              text-align: center;
+              display: inline-block;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">
+                  Hello Laravel   !--by bella
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
